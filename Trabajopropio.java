6daee45e6decc1e254964e4b55a1c526b1fe4153
ivan9989-pkj <santanaquintanai@gmail.com>
2023-04-10 15:24:34 +0100

    first commit

---
+++ Trabajopropio.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.trabajopropio;
+
+/**
+ * Aqui es donde se generaran todos los datos 
+ * @author Ivan Santana
+ * 
+ */
+
+public class Trabajopropio {
+
+    /**
+     * Es un main
+     * @param args  es el que hara todas las operaciones
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
